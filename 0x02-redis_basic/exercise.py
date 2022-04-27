3f9445cb94eb11397faef0590e4f6ba1b500a28f
Janengethe <janengethew@gmail.com>
2022-04-27 17:01:41 +0300

    task 2

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -6,6 +6,36 @@ Contains class Cache
 import redis
 import uuid
 from typing import Union, Optional, Callable
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    """
+    implement a system to count how many times methods of
+    the Cache class are called.
+    takes a single method Callable argument and returns a Callable.
+    As a key, use the qualified name of method using the
+    __qualname__ dunder method.
+    """
+    key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self, *args, **kwds):
+        """
+        Creates a return function that increments the count for that key
+        every time the method is called and returns the value returned
+        by the original method.
+
+        the first argument of the wrapped function is self which is the
+        instance itself, which lets you access the Redis instance.
+
+        Protip: https://docs.python.org/3.7/library/functools.html
+        #functools.wraps
+        """
+        self._redis.incr(key)
+        return method(self, *args, **kwds)
+
+    return wrapper
 
 
 class Cache:
@@ -14,6 +44,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         method generate a random key (e.g. using uuid),
