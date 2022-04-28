18b150dc6af1062eea29306d946010b1ea56d8b6
Janengethe <janengethew@gmail.com>
2022-04-28 05:28:07 +0300

    typing

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -94,7 +94,7 @@ class Cache:
         return int(data, "base=0")
 
 
-def replay(method: Callable):
+def replay(method: Callable) -> None:
     """ display the history of calls for a function """
     key = method.__qualname__
     inputs = key + ":inputs"
